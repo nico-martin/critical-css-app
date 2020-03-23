9de46c9a429795911c03f4bbaabbf91d4e775fdd
nico_martin <mail@nicomartin.ch>
2020-03-23 08:57:12 +0100

    made ProjectsTokenInput copyable

--- src/components/Dashboard/Projects/TokenInput.jsx
+++ src/components/Dashboard/Projects/TokenInput.jsx
@@ -5,8 +5,8 @@ import { h } from 'preact';
 const ProjectsTokenInput = ({ token, ...rest }: { token: string }) => (
   <input
     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-    disabled={true}
     value={token}
+    onKeyUp={e => (e.target.value = token)}
     {...rest}
   />
 );
